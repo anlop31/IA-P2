4d92375d3f80d25697708fd7b1533549373316cf
anlop31 <anlop31@correo.ugr.es>
2024-04-24 18:19:39 +0200

    avanzado tutorial

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -13,7 +13,29 @@ Action ComportamientoJugador::think(Sensores sensores)
 {
 	Action accion = actIDLE;
 
-	// Incluir aquí el comportamiento del agente jugador
+
+	if (!hayPlan) {
+		// Invocar al método de búsqueda
+		c_state.jugador.f = sensores.posF;
+		c_state.jugador.c = sensores.posC;
+		c_state.jugador.brujula == sensores.sentido;
+		c_state.colaborador.f = sensores.CLBposF;
+		c_state.colaborador.c = sensores.CLBposC;
+		c_state.colaborador.brujula = sensores.CLBsentido;
+		goal.f = sensores.destinoF;
+		goal.c = sensores.destinoC;
+
+		hayPlan = anchuraSoloJugador(c_state, goal, mapaResultado);
+		if (hayPlan) cout << "Se encontro un plan" << endl;
+	}
+	if (hayPlan and plan.size() > 0) {
+		accion = plan.front();
+		plan.pop_front();
+	}
+	if (plan.size() == 0) {
+		cout << "Se completo el plan\n";
+		hayPlan = false;
+	}
 
 	return accion;
 }
@@ -22,4 +44,236 @@ Action ComportamientoJugador::think(Sensores sensores)
 int ComportamientoJugador::interact(Action accion, int valor)
 {
 	return false;
+}
+
+
+list<Action> ComportamientoJugador::AvanzaASaltosDeCaballo() {
+	list<Action> secuencia;
+
+	secuencia.push_back(actWALK);
+	secuencia.push_back(actWALK);
+	secuencia.push_back(actTURN_L);
+	secuencia.push_back(actWALK);
+
+	return secuencia;
+}
+
+/** primera aproximacion a la implementacion de la busqueda en anchura **/
+bool ComportamientoJugador::anchuraSoloJugador (const stateN0 &inicio, const ubicacion &final,
+						 const vector<vector<unsigned char>> &mapa)
+{
+	stateN0 current_state = inicio; 	// Estado actual
+	list<stateN0> frontier;				// Lista de estados pendientes de explorar
+	list<stateN0> explored;				// Lista de estados ya explorados
+	frontier.push_back(current_state);	
+
+
+	// Variable que determina si ya se ha encontrado un estado que satisface la 
+	// condicion de ser solucion.
+	bool solutionFound = (current_state.jugador.f == final.f and
+						  current_state.jugador.c == final.c);
+
+	while (!frontier.empty() and !solutionFound) {
+		frontier.pop_front();
+		explored.push_back(current_state);
+
+		// Generar hijo actWALK
+		stateN0 child_walk = apply(actWALK, current_state, mapa);
+
+		if (child_walk.jugador.f == final.f and child_walk.jugador.c == final.c) {
+			current_state = child_walk;
+			solutionFound = true;
+
+		}
+		else if (!Find(child_walk, frontier) and !Find(child_walk, explored)) {
+			frontier.push_back(child_walk);
+		}
+
+		if (!solutionFound) {
+			// Generar hijo actRUN
+			stateN0 child_run = apply(actRUN, current_state, mapa);
+			
+			if (child_run.jugador.f == final.f and child_run.jugador.c == final.c) {
+				current_state = child_run;
+				solutionFound = true;
+			}
+			else if (!Find(child_run, frontier) and !Find(child_run, explored)) {
+				frontier.push_back(child_run);
+			}
+		}
+
+		if (!solutionFound) {
+			// Generar hijo actTURN_L
+			stateN0 child_turnl = apply(actTURN_L, current_state, mapa);
+			
+			if (!Find(child_turnl, frontier) and !Find(child_turnl, explored)) {
+				frontier.push_back(child_turnl);
+			}
+
+			// Generar hijo actTURN_SR
+			stateN0 child_turnsr = apply(actTURN_SR, current_state, mapa);
+			
+			if (!Find(child_turnsr, frontier) and !Find(child_turnsr, explored)) {
+				frontier.push_back(child_turnsr);
+			}
+		}
+
+		if (!solutionFound and !frontier.empty()) {
+			current_state = frontier.front();
+		}
+
+	}
+
+	return solutionFound;
+}
+
+/* Devuelve si una ubicación en el mapa es transitable para el agente */
+bool ComportamientoJugador::casillaTransitable (const ubicacion &x, const vector<vector<unsigned char>> &mapa) {
+	return (mapa[x.f][x.c] != 'P' and mapa[x.f][x.c] != 'M');
+}
+
+/* Devuelve la ubicación siguiente según el avance del agente */
+ubicacion ComportamientoJugador::nextCasilla (const ubicacion &pos) {
+	ubicacion next = pos;
+
+	switch (pos.brujula)
+	{
+		case norte:
+			next.f = pos.f - 1;
+			break;
+		case noreste:
+			next.f = pos.f - 1;
+			next.c = pos.c + 1;
+			break;
+		case este:
+			next.c = pos.c + 1;
+			break;
+		case sureste:
+			next.f = pos.f + 1;
+			next.c = pos.c + 1;
+			break;
+		case sur:
+			next.f = pos.f + 1;
+			break;
+		case suroeste:
+			next.f = pos.f + 1;
+			next.c = pos.c - 1;
+			break;
+		case oeste:
+			next.c = pos.c - 1;
+			break;
+		case noroeste:
+			next.f = pos.f - 1;
+			next.c = pos.c - 1;
+			break;
+	}
+
+	return next;
+}
+
+/* Devuelve el estado que se genera si el agente puede avanzar.
+   Si no puede avanzar, devuelve como salida el mismo estado de entrada. */
+stateN0 ComportamientoJugador::apply (const Action &a, const stateN0 &st, const vector<vector<unsigned char>> mapa) {
+	stateN0 st_result = st;
+	ubicacion sig_ubicacion, sig_ubicacion2;
+
+	switch (a) {
+		case actWALK: // si prox casilla es transitable y no está ocupada por el colaborador
+			sig_ubicacion = nextCasilla(st.jugador);
+			if (casillaTransitable(sig_ubicacion, mapa) and
+					!(sig_ubicacion.f == st.colaborador.f and sig_ubicacion.c == st.colaborador.c)) {
+						st_result.jugador = sig_ubicacion;
+					}
+			break;
+		case actRUN: // si prox 2 casillas son transitables y no está ocupada por el colaborador
+			sig_ubicacion = nextCasilla(st.jugador);
+			if (casillaTransitable(sig_ubicacion, mapa) and	
+					!(sig_ubicacion.f == st.colaborador.f and sig_ubicacion.c == st.colaborador.c)) {
+						sig_ubicacion2 = nextCasilla(sig_ubicacion);
+						if (casillaTransitable(sig_ubicacion2, mapa) and
+							!(sig_ubicacion2.f == st.colaborador.f and sig_ubicacion2.c == st.colaborador.c)) {
+								st_result.jugador = sig_ubicacion2;
+							}
+					}
+			break;
+		case actTURN_L:
+			st_result.jugador.brujula = static_cast<Orientacion>((st_result.jugador.brujula+6) % 8);
+			break;
+		case actTURN_SR:
+			st_result.jugador.brujula = static_cast<Orientacion>((st_result.jugador.brujula+1) % 8);
+			break;
+	}
+
+
+	return st_result;
+}
+
+/* Encuentra si el elemento item está en la lista */
+bool ComportamientoJugador::Find (const stateN0 &item, const list<stateN0> &lista) {
+	auto it = lista.begin();
+	while ( it != lista.end() and !( (*it) == item ) ) {
+		it++;
+	}
+
+	return ( !( it == lista.end() ) );
+}
+
+void ComportamientoJugador::anularMatriz (vector<vector<unsigned char>> &matriz) {
+	for (int i = 0; i < matriz.size(); i++) {
+		for (int j = 0; j < matriz[0].size(); j++) {
+			matriz[i][j] = 0;
+		}
+	}
+}
+
+void ComportamientoJugador::visualizarPlan (const stateN0 &st, const list<Action> &plan) {
+	anularMatriz(mapaConPlan);
+	stateN0 cst = st;
+
+	auto it = plan.begin();
+	
+	while (it != plan.end()) {
+		if ( (*it != act_CLB_WALK) and (*it != act_CLB_TURN_SR) and (*it != act_CLB_STOP) )
+		{
+			switch (cst.ultimaOrdenColaborador)
+			{
+			case act_CLB_WALK:
+				cst.colaborador = nextCasilla(cst.colaborador);
+				mapaConPlan[cst.colaborador.f][cst.colaborador.c] = 2;
+				break;
+			case act_CLB_TURN_SR:
+				cst.colaborador.brujula = (Orientacion)((cst.colaborador.brujula + 1) % 8);
+				break;
+			}
+		}
+
+		switch (*it)
+		{
+		case actRUN:
+			cst.jugador = nextCasilla(cst.jugador);
+			mapaConPlan[cst.jugador.f][cst.jugador.c] = 3;
+			cst.jugador = nextCasilla(cst.jugador);
+			mapaConPlan[cst.jugador.f][cst.jugador.c] = 1;
+			break;
+		case actWALK:
+			cst.jugador = nextCasilla(cst.jugador);
+			mapaConPlan[cst.jugador.f][cst.jugador.c] = 1;
+			break;
+		case actTURN_SR:
+			cst.jugador.brujula = (Orientacion)((cst.jugador.brujula + 1) %  8);
+			break;
+		case actTURN_L:
+			cst.jugador.brujula = (Orientacion)((cst.jugador.brujula + 6) %  8);
+			break;
+		case act_CLB_TURN_SR:
+			cst.jugador.brujula = (Orientacion)((cst.colaborador.brujula + 1) %  8);
+			cst.ultimaOrdenColaborador = act_CLB_TURN_SR;
+			break;
+		case act_CLB_STOP:
+			cst.ultimaOrdenColaborador = act_CLB_STOP;
+			break;
+		}
+		it++;
+	}
+	
 }
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -5,6 +5,42 @@
 
 #include <list>
 
+struct stateN0 {
+  ubicacion jugador;
+  ubicacion colaborador;
+  Action ultimaOrdenColaborador;
+
+  bool operator==(const stateN0 &x) const {
+    if (jugador == x.jugador and
+          colaborador.f == x.colaborador.f and colaborador.c == x.colaborador.c) {
+            return true;
+    }
+    else {
+      return false;
+    }
+  }
+};
+
+struct nodeN0 {
+  stateN0 st;
+  list<Action> secuencia;
+
+  bool operator==(const nodeN0 &n) const {
+    return (st == n.st);
+  }
+
+  bool operator<(const nodeN0 &b) const {
+    if (st.jugador.f < st.jugador.f)
+      return true;
+    else if (st.jugador.f == b.st.jugador.f and st.jugador.c < b.st.jugador.c)
+      return true;
+    else if (st.jugador.f == b.st.jugador.f and st.jugador.c == b.st.jugador.c and st.jugador.brujula < b.st.jugador.brujula)
+      return true;
+    else
+      return false;
+  }
+};
+
 
 class ComportamientoJugador : public Comportamiento {
   public:
@@ -13,19 +49,31 @@ class ComportamientoJugador : public Comportamiento {
     }
     ComportamientoJugador(std::vector< std::vector< unsigned char> > mapaR) : Comportamiento(mapaR) {
       // Inicializar Variables de Estado
+      hayPlan = false;
     }
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
     ~ComportamientoJugador(){}
 
     Action think(Sensores sensores);
     int interact(Action accion, int valor);
+    list<Action> AvanzaASaltosDeCaballo();
+    bool anchuraSoloJugador (const stateN0 &inicio, const ubicacion &final, const vector<vector<unsigned char>> &mapa);
+    bool casillaTransitable (const ubicacion &x, const vector<vector<unsigned char>> &mapa);
+    ubicacion nextCasilla (const ubicacion &pos);
+    stateN0 apply (const Action &a, const stateN0 &st, const vector<vector<unsigned char>> mapa);
+    bool Find (const stateN0 &item, const list<stateN0> &lista);
+    void anularMatriz (vector<vector<unsigned char>> &matriz);
+    void visualizarPlan (const stateN0 &st, const list<Action> &plan);
+
+
 
 
   private:
     // Declarar Variables de Estado
-
-
-
+    list<Action> plan; // Almacena el plan en ejecución
+    bool hayPlan;      // Si es true indica que se está siguiendo un plan
+    stateN0 c_state;
+    ubicacion goal;
 
 
 };
